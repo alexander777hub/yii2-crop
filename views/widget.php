a4eff4f8e02876d5e4071e2021a438a8524208d4
alexander777hub <aleksandrpereselkov77@gmail.com>
2022-07-08 15:23:26 +0200

    fix

--- views/widget.php
+++ views/widget.php
@@ -26,6 +26,12 @@ use yii\helpers\Html;
 
 ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<style>
+    .modal-body {
+
+        padding: 0rem !important;
+    }
+</style>
 <div class="row">
     <div class="col-12 col-sm-6">
         <div class="bg-light js-upload-item" id="new_public_photo">
